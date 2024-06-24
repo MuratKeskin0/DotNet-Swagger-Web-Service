bcc7e20aee05e71e93e387e4732a8af45f116ff1
Murat <eng.murat.keskin@gmail.com>
2024-06-24 14:47:20 +0300

    .

---
+++ WebApplicationMotorBazaar/webservices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webservices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
